c50ff6dc7eedb4754fac0f7336f33fc9814aec91
Yvan Sraka <yvan@sraka.xyz>
2022-12-04 17:49:25 +0100

    Day 4 (Nix without `nixpkgs`)

---
+++ 4b.nix
@@ -0,0 +1,6 @@
+with builtins;
+trace (length (filter (xs:
+  ((elemAt xs 3) <= (elemAt xs 0) && (elemAt xs 1) <= (elemAt xs 2))
+  || ((elemAt xs 0) <= (elemAt xs 3) && (elemAt xs 2) <= (elemAt xs 1)))
+  (map (map (n: fromJSON "${n}")) (filter (x: !(isString x))
+    (split "([0-9]*)-([0-9]*),([0-9]*)-([0-9]*)" (readFile ./input.txt)))))) { }
